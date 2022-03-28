0663be5d496ccc5028f8c32979f53b652ea79b2f
Ovchinnikov Kirill <Ovchinnikovk758@gmail.com>
2022-03-28 23:46:21 +0300

    all markup

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,8 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
+import EmployersList from '../employers-list/employers-list';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
 import './app.css';
 
 function App() {
@@ -12,6 +14,9 @@ function App() {
                 <SearchPanel/>
                 <AppFilter/>
             </div>
+
+            <EmployersList/>
+            <EmployersAddForm/>
         </div>
     )
 }
